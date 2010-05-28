bf1991ee23b0aab30c92c182987c8ab4811bd0b8
Masanori KADO <kdmsnr@gmail.com>
2010-05-28 21:12:38 +0900

    add license, usage, 3tab

--- tdiary_twitter/tdiary_twitter.rb
+++ tdiary_twitter/tdiary_twitter.rb
@@ -1,28 +1,35 @@
+# You can redistribute it and/or modify it under the same license as tDiary.
+#
+# USAGE
+#   title: {{twitter_title 12425500138}}
+#   tweet: {{twitter_tweet 12425500138}} or {{twitter 12425500138}}
+#   detail: {{twitter_detail 12425500138}}
+
 require 'rubygems'
 require 'open-uri'
 require 'json'
 require 'time'
 
 def twitter_title( id )
-  twitter_format(id, "@:screen_name / :text")
+	twitter_format(id, "@:screen_name / :text")
 end
 
 def twitter_tweet( id )
-  format =<<-EOS
+	format =<<-EOS
 @:screen_name: :text<br />
 :created_at
 :source
 EOS
-  twitter_format(id, format)
+	twitter_format(id, format)
 end
 
 def twitter_detail( id )
-  format =<<-EOS
+	format =<<-EOS
 :profile_image :text<br />
 :created_at
 :source
 EOS
-  twitter_format( id , format)
+	twitter_format( id , format)
 end
 
 # TBD
@@ -30,21 +37,30 @@ end
 # end
 
 def twitter_format( id, format = "@:screen_name / :text" )
-  json =   open("http://twitter.com/status/show/#{id}.json").read
-  parsed = JSON.parse( json )
+	json =   open("http://twitter.com/status/show/#{id}.json").read
+	parsed = JSON.parse( json )
 
-  format.gsub!(/:screen_name/, parsed["user"]["screen_name"])
-  format.gsub!(/:text/, parsed["text"])
-  format.gsub!(/:source/, parsed["source"])
-  t = Time.parse parsed["created_at"]
-  format.gsub!(/:created_at/,
-               %Q|<a href="http://twitter.com/#{parsed["user"]["screen_name"]}/status/#{id}">#{t.strftime("%Y-%m-%d %H:%M:%S")}</a>|)
-  format.gsub!(/:profile_image/,
-               %Q|<a href="http://twitter.com/#{parsed["user"]["screen_name"]}"><img src="#{parsed["user"]["profile_image_url"]}" border="0" /></a>|)
+	format.gsub!(/:screen_name/, parsed["user"]["screen_name"])
+	format.gsub!(/:text/, parsed["text"])
+	format.gsub!(/:source/, parsed["source"])
+	t = Time.parse parsed["created_at"]
+	format.gsub!(/:created_at/,
+					 %Q|<a href="http://twitter.com/#{parsed["user"]["screen_name"]}/status/#{id}">#{t.strftime("%Y-%m-%d %H:%M:%S")}</a>|)
+	format.gsub!(/:profile_image/,
+					 %Q|<a href="http://twitter.com/#{parsed["user"]["screen_name"]}"><img src="#{parsed["user"]["profile_image_url"]}" border="0" /></a>|)
 
-  return %Q|<div class="twitter">#{format.chomp}</div>|
+	return %Q|<div class="twitter">#{format.chomp}</div>|
 end
 
 def twitter( id )
-  twitter_tweet( id )
+	twitter_tweet( id )
 end
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# End:
+# vim: ts=3
+
--- tdiary_twitter/test_tdiary_twitter.rb
+++ tdiary_twitter/test_tdiary_twitter.rb
@@ -5,25 +5,34 @@ require 'redgreen'
 require 'tdiary_twitter'
 
 class TestTwitter < Test::Unit::TestCase
-  should "twitter_title" do
-    assert_equal('<div class="twitter">@tDiary / そういえば、明日で9歳になります!</div>', twitter_title(12425500138))
-  end
+	should "twitter_title" do
+		assert_equal('<div class="twitter">@tDiary / そういえば、明日で9歳になります!</div>', twitter_title(12425500138))
+	end
 
-  should "twitter_tweet" do
-    expect =<<-EOS
+	should "twitter_tweet" do
+		expect =<<-EOS
 <div class="twitter">@tDiary: そういえば、明日で9歳になります!<br />
 <a href="http://twitter.com/tDiary/status/12425500138">2010-04-19 09:04:09</a>
 <a href="http://simplytweet.com" rel="nofollow">SimplyTweet</a></div>
 EOS
-    assert_equal(expect.chomp, twitter_tweet(12425500138))
-  end
+		assert_equal(expect.chomp, twitter_tweet(12425500138))
+	end
 
-  should "twitter_detail" do
-    expect =<<-EOS
+	should "twitter_detail" do
+		expect =<<-EOS
 <div class="twitter"><a href="http://twitter.com/tDiary"><img src="http://a3.twimg.com/profile_images/379668735/square_logo_normal.png" border="0" /></a> そういえば、明日で9歳になります!<br />
 <a href="http://twitter.com/tDiary/status/12425500138">2010-04-19 09:04:09</a>
 <a href="http://simplytweet.com" rel="nofollow">SimplyTweet</a></div>
 EOS
-    assert_equal(expect.chomp, twitter_detail(12425500138))
-  end
+		assert_equal(expect.chomp, twitter_detail(12425500138))
+	end
 end
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# End:
+# vim: ts=3
+
