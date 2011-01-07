f4e6b6970cb9d96ca394abb8e736cb27d4d4db81
KADO Masanori <kdmsnr@gmail.com>
2011-01-07 13:16:26 +0900

    add zenback plugin

---
+++ zenback/zenback.rb
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+if @mode == 'day'
+	add_body_leave_proc do
+		@conf['zenback.script'] || ''
+	end
+end
+
+add_conf_proc( 'zenback', 'zenback', 'etc' ) do
+	if @mode == 'saveconf' then
+		@conf['zenback.script'] = @cgi.params['zenback.script'][0]
+	end
+
+<<-HTML
+   <h3>Script Code</h3>
+   <p><input size="88" name="zenback.script" value="#{@conf['zenback.script']}"></p>
+HTML
+end
+
+# Local Variables:
+# mode: ruby
+# indent-tabs-mode: t
+# tab-width: 3
+# ruby-indent-level: 3
+# End:
+# vim: ts=3
+
