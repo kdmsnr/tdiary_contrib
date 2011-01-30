c2891e57cca956e866ad278328affe77065f552d
KADO Masanori <kdmsnr@gmail.com>
2011-01-31 02:43:47 +0900

    title tag

--- section_link/misc/plugin/section_link.rb
+++ section_link/misc/plugin/section_link.rb
@@ -16,8 +16,8 @@ def title_tag
 			sec << s
 		end
 		site_title = " - #{_orig_title_tag.gsub( /<.*?>/, '')}"
-		title = sec[@cgi.params['section'][0].to_i - 1].subtitle
-		return "<title>#{h title}#{h site_title }</title>"
+		title = sec[@cgi.params['section'][0].to_i - 1].stripped_subtitle_to_html
+		return "<title>#{apply_plugin(title, true).gsub(/[\r\n]/, '')}#{h site_title }</title>"
 
 	else
 		_orig_title_tag
