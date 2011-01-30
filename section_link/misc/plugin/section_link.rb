8a09e673076f510d1698bc5d1f145158acd4b555
KADO Masanori <kdmsnr@gmail.com>
2011-01-30 22:31:34 +0900

    add title tag

--- section_link/misc/plugin/section_link.rb
+++ section_link/misc/plugin/section_link.rb
@@ -7,6 +7,24 @@ def subtitle_link( date, index, subtitle )
 	r.gsub(/#p(\d+)/){"&section=#{$1}"}
 end
 
+alias :_orig_title_tag :title_tag
+def title_tag
+	if @cgi.params['section'].join('') != '' and
+			@mode == 'day' and diary = @diaries[@date.strftime('%Y%m%d')]
+		sec = []
+		diary.each_section do |s|
+			sec << s
+		end
+		site_title = " - #{_orig_title_tag.gsub( /<.*?>/, '')}"
+		title = sec[@cgi.params['section'][0].to_i - 1].subtitle
+		return "<title>#{h title}#{h site_title }</title>"
+
+	else
+		_orig_title_tag
+	end
+end
+
+
 # Local Variables:
 # mode: ruby
 # indent-tabs-mode: t
