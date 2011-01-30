96202b474af25674c65cf7702695d644a71f8573
KADO Masanori <kdmsnr@gmail.com>
2011-01-30 22:10:10 +0900

    fix index

--- section_link/misc/plugin/section_link.rb
+++ section_link/misc/plugin/section_link.rb
@@ -1,5 +1,8 @@
 alias :_orig_subtitle_link :subtitle_link
 def subtitle_link( date, index, subtitle )
+	if @cgi.params['section'].join('') != ''
+		index = @cgi.params['section'][0]
+	end
 	r = _orig_subtitle_link( date, index, subtitle )
 	r.gsub(/#p(\d+)/){"&section=#{$1}"}
 end
